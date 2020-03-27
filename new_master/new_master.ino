0f4ec8951af3b90b94fa0fb10703d5edec319f30
rampfire <abishek.ur@gmail.com>
2020-03-27 22:40:15 +0530

    latest changes tryiung to add the time functionality

--- new_master/new_master.ino
+++ new_master/new_master.ino
@@ -1,3 +1,7 @@
+#include <SoftwareSerial.h>
+#include <Wire.h>
+#include <TimeLib.h>
+#include <DS1307RTC.h>
 #include <avr/wdt.h>
 const int water_level_full = 4;
 const int water_level_medium=3;
@@ -10,21 +14,23 @@ int motor_off_relay=9;
 const int manual_Start_auto_Off_switch=10;
 const int semi_auto_mode_light=11;
 const int auto_mode_light=12;
-const int water_running_light=A0;
+const int voltage_input_pin=A0;
 String tank_level="NN";
-int f=0;
-int m=0;
+//int f=0;
+//int m=0;
 String mode_0f_Operation="auto";
 unsigned long startTimerTime=0; 
 unsigned long stopTimerTime=0;
-const int  max_motor_running_time=1500000;//max time the motor should run regardless of what the situation is.
-#include <SoftwareSerial.h>
+const long max_motor_running_time=1500000;//max time the motor should run regardless of what the situation is.
+
 //
 int motor_status=0;//the motor status at any given time.this is a digital variable meaning the physical running of the motor is not specified.
 int red_flag =0;//any parameters out of range then this red_flag is raised and anywhere in the program this occurs the motor is turned down.
+int voltage_flag=1;//0 means voltage is ok 1 means voltage is not ok
 
 int initialize=1;
 
+String time_flag="NN";
 void setup()
 {
   wdt_disable();//watch_dog timer is disabled here so that if enabled elsewhere it gets nullified
@@ -43,8 +49,6 @@ void setup()
   digitalWrite(semi_auto_mode_light,LOW);
   pinMode(auto_mode_light, OUTPUT);
   digitalWrite(auto_mode_light,LOW);
-  pinMode(water_running_light, OUTPUT);
-  digitalWrite(water_running_light,LOW);
   delay(5000);
   Serial.println("program started and successfully connected to SERIAL monitor");
   delay(5000);//...........??????provide a certain delay before the motor starts so that the voltages settle
@@ -53,6 +57,7 @@ void setup()
   pinMode(water_level_intermediate, INPUT);
   pinMode(water_sensor, INPUT);
   pinMode(manual_Start_auto_Off_switch, INPUT);
+  pinMode(voltage_input_pin,INPUT);
   
   wdt_enable(WDTO_4S);
 }
@@ -77,16 +82,16 @@ void loop()
   wdt_reset();
   initialize=0;
   }
-  
+  voltage_check_function();
   
   if(red_flag==1){
     digitalWrite(red_flag_led,HIGH);
   }
-  if((motor_status==1) and ((red_flag==1) or (tank_level=="full")) ){
+  if((motor_status==1) and ((red_flag==1) or (tank_level=="full") or ((millis()-startTimerTime)>max_motor_running_time) or (voltage_flag==1)) ){
      motor_stop();
      Serial.println("motor has stopped");
   }
-  if((motor_status==0) and (red_flag==0) and (tank_level=="low") and (mode_0f_Operation=="auto")){
+  if((motor_status==0) and (red_flag==0) and (tank_level=="low") and (mode_0f_Operation=="auto") and (voltage_flag==0) and (time_flag=="go_ahead")){
     ///start the motor if everything is ok 
       motor_start();
       delay(3000);
@@ -104,15 +109,14 @@ void loop()
       Serial.println("dry run check completed");
   } 
   Serial.println(mode_0f_Operation);
-  if(digitalRead(water_sensor)==HIGH){
-    digitalWrite(water_running_light,HIGH);
-  }
   semi_0r_auto_light();
 //  if(motor_status==0 and (millis()-stopTimerTime)>60000){
 //      Serial.println("dry run check initiated");
 //      red_flag=0;
 //      Serial.println("dry run check completed");
 //  }
+//  read_time_and_set_variable();
+//  Serial.println(time_flag);
 }
 /////
 void motor_start(){
@@ -212,9 +216,62 @@ void semi_0r_auto_light(){
   if (mode_0f_Operation=="auto"){
     
     digitalWrite(auto_mode_light,HIGH);
+    digitalWrite(semi_auto_mode_light,LOW);
     
   }
   if (mode_0f_Operation=="semi_auto"){
     digitalWrite(semi_auto_mode_light,HIGH);
+    digitalWrite(auto_mode_light,LOW);
   }
 }
+void voltage_check_function(){
+  if (digitalRead(voltage_input_pin)==HIGH){
+    voltage_flag=0;
+  }
+  else if(digitalRead(voltage_input_pin)==LOW){
+    voltage_flag=1;
+  }
+  Serial.println("voltage check function executed"+String(voltage_flag));
+}
+//void read_time_and_set_variable() {
+//  tmElements_t tm;
+//
+//  if (RTC.read(tm)) {
+//    Serial.print("Ok, Time = ");
+//    print2digits(tm.Hour);
+//    Serial.write(':');
+//    print2digits(tm.Minute);
+//    Serial.write(':');
+//    print2digits(tm.Second);
+//    Serial.print(", Date (D/M/Y) = ");
+//    Serial.print(tm.Day);
+//    Serial.write('/');
+//    Serial.print(tm.Month);
+//    Serial.write('/');
+//    Serial.print(tmYearToCalendar(tm.Year));
+//    Serial.println();
+//  } else {
+//    if (RTC.chipPresent()) {
+//      Serial.println("The DS1307 is stopped.  Please run the SetTime");
+//      Serial.println("example to initialize the time and begin running.");
+//      Serial.println();
+//    } else {
+//      Serial.println("DS1307 read error!  Please check the circuitry.");
+//      Serial.println();
+//    }
+//    delay(9000);
+//  }
+//  if (tm.Hour>=14 and tm.Hour<=24 and tm.Minute>=1 and tm.Minute>=1){
+//    time_flag="go_ahead";
+//  }
+//  else{
+//    time_flag="NN";
+//  }
+//}
+//
+//void print2digits(int number) {
+//  if (number >= 0 && number < 10) {
+//    Serial.write('0');
+//  }
+//  Serial.print(number);
+//}
